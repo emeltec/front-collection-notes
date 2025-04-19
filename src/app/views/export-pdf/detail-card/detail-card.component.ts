fa4ce51319300d9a6d9f1b97468c54150484b8a2
Emilio Melgarejo <emelgarejox@gmail.com>
2025-04-19 13:42:36 -0500

    export pdf

---
+++ src/app/views/export-pdf/detail-card/detail-card.component.ts
@@ -0,0 +1,16 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { DetailsCard } from '../utils/detail-card.interface';
+
+@Component({
+  selector: 'app-detail-card',
+  imports: [NgIf, NgFor, NgClass],
+  templateUrl: './detail-card.component.html',
+  styleUrls: ['./detail-card.component.scss']
+})
+export class DetailCardComponent {
+  @Input() detailsCard: DetailsCard[] = [];
+
+  constructor() { }
+  
+}
